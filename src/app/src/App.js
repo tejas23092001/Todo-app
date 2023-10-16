e7b5fc3900554279938d7c2707cc43f75de319d7
root <root@ShadowClone>
2023-10-16 21:28:31 +0530

    change a bit of UI

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,29 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
-const containerStyle = {
-  textAlign: 'center',
-
-  padding: '20px',
-  width: '50%',
-  margin: "20px"
-};
-
-const formStyle = {
-  margin: '20px',
-};
-
-const listStyle = {
-  listStyle: 'none',
-  padding: '0',
-};
-
-const itemStyle = {
-  border: '1px solid #ccc',
-  margin: '5px',
-  padding: '10px',
-  borderRadius: '5px',
-  backgroundColor: '#f9f9f9',
-};
+import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -59,34 +35,24 @@ function App() {
   };
 
   return (
-    <div style={{
-      width: "100%",
-      height: "100%",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "rgb(203, 195, 227)",
-    }}>
-
-      <div style={containerStyle}>
-        <h1>Todo List</h1>
-        <form style={formStyle} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Add a new todo"
-            value={newTodo}
-            onChange={handleInputChange}
-          />
-          <button type="submit">Add</button>
-        </form>
-        <ul style={listStyle}>
-          {todos.map((todo) => (
-            <li key={todo._id} style={itemStyle}>
-              {todo.description}
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className='containerStyle'>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a new todo"
+          value={newTodo}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            {todo.description}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
